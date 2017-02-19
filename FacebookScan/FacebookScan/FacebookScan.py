da2d6ffd7e28dfd320ce4638fa2e426e346c6170
Michael Skelton <michaelskelton@cancerqld.org.au>
2017-02-19 21:44:03 +1000

    Base logo and framework

---
+++ FacebookScan/FacebookScan/FacebookScan.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+import multiprocessing
+import subprocess
+from argparse import ArgumentParser
+import sys
+
+def print_logo():
+    print(".-. .-. .-. .-. .-. .-. .-. . .   .-. .-. .-. . .")
+    print("|-  |-| |   |-  |(  | | | | |<    `-. |   |-| |\|")
+    print("'   ` ' `-' `-' `-' `-' `-' ' `   `-' `-' ` ' ' `")
+
+def main():
+    print_logo()
+
+
+if __name__ == "__main__":
+    main()
