d560fa28a28fc22a87bc2996e9d4c5ccf4f6ce4b
Michael Skelton <michaelskelton@cancerqld.org.au>
2017-02-19 22:04:56 +1000

    Base argument parsers

--- FacebookScan/FacebookScan/FacebookScan.py
+++ FacebookScan/FacebookScan/FacebookScan.py
@@ -11,7 +11,26 @@ def print_logo():
 
 def main():
     print_logo()
+    
+    parser = ArgumentParser()
+    parser.add_argument("-url", dest="url", help="Provide a url for a facebook profile to scan.")
+    parser.add_argument("-id", dest="id", help="Provide an id for a facebook profile to scan.")
+    parser.add_argument("--printid", dest="printid",  action="store_true", help="Retreive and print the id of a provided facebook url")
 
+    arguments = parser.parse_args()
+
+    if len(sys.argv) == 1:
+        parser.error("No arguments given.  Please specify a profile (url or id) as well as the type of queries to generate.")
+        sys.exit()
+
+    if arguments.id is None and arguments.url is None:
+        print("No facebook id or url specified, please supply a url")
+
+    if arguments.url is not None:
+        print("Profile url is: {}".format(arguments.url))
+
+    if arguments.id is True:
+        print("Facebook profile id is:")
 
 if __name__ == "__main__":
     main()
